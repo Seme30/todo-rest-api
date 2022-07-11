569b3f7d9b1beba264d39f218e4b33cac60fa1f6
Semahegn <83661382+Seme30@users.noreply.github.com>
2022-07-11 16:48:16 +0300

    todoItem model

---
+++ src/main/java/com/seme30/todo/models/todoItem.java
@@ -0,0 +1,53 @@
+package com.seme30.todo.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "TodoList")
+public class todoItem {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Getter
+    @Setter
+    @Column(name="title")
+    private String todoTitle;
+
+    @Getter
+    @Setter
+    @Column(name="description")
+    private String todoDescription;
+
+
+    @Getter
+    @Setter
+    @Column(name="type")
+    private String todoType;
+
+    @Getter
+    @Setter
+    @Column(name="deadline")
+    private Date todoDeadline;
+
+    @Getter
+    @Setter
+    @Column(name="status")
+    private boolean status;
+
+
+
+    
+}
