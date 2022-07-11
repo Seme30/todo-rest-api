6d5be96558a3e4d651b0427e88a91ed94ce084d7
Semahegn <83661382+Seme30@users.noreply.github.com>
2022-07-11 17:00:27 +0300

    todoitem service implementation

---
+++ src/main/java/com/seme30/todo/services/serviceImpl/TodoItemServiceImpl.java
@@ -0,0 +1,26 @@
+package com.seme30.todo.services.serviceImpl;
+
+import java.util.List;
+
+import com.seme30.todo.models.todoItem;
+import com.seme30.todo.repository.TodoItemRepository;
+import com.seme30.todo.services.TodoItemService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoItemServiceImpl implements TodoItemService{
+
+    private TodoItemRepository todoItemRepository;
+
+    @Override
+    public todoItem saveTodo(todoItem todo) {
+        return todoItemRepository.save(todo);
+    }
+
+    @Override
+    public List<todoItem> getAllTodos() {
+        return todoItemRepository.findAll();
+    }
+    
+}
